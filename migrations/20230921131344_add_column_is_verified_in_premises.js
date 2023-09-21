449ace14125a98ceeb1f33b9a103c76a74e20629
Luqman Yusof <lockman_akim@yahoo.com>
2023-09-22 01:01:40 +0800

    account verification - positive flow is done

---
+++ migrations/20230921131344_add_column_is_verified_in_premises.js
@@ -0,0 +1,17 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.table('premises', table => {
+    table.boolean('is_verified').notNullable().after('is_active').defaultTo(0);
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  
+};
--- routes/premises.js
+++ routes/premises.js
@@ -12,49 +12,48 @@ if (config.env == 'prod') {
   knex = require('knex').knex(dbConfig.prod);
 }
 
-router.get('/add', function(req, res, next) {
-  res.render('premises/add', { errors: []});
+router.get('/add', function (req, res, next) {
+  res.render('premises/add', { errors: [] });
 });
 
-router.get('/login', function(req, res, next) {
+router.get('/login', function (req, res, next) {
   res.render('premises/login');
 });
 
-router.post('/add', 
+router.post('/add',
   body('tbxBusiness').notEmpty().trim().withMessage('Business name is required.'),
   body('tbxPhone').notEmpty().trim().isMobilePhone('ms-MY').withMessage('Please insert valid phone number'),
-  body('tbxPassword').notEmpty().isStrongPassword({minLength: 8, minUppercase: 1, minNumbers: 1, minSymbols: 1}).withMessage('Please use strong password'),
-  body('tbxConfirmPassword').notEmpty().custom((value, {req}) => { return req.body.tbxPassword === req.body.tbxConfirmPassword }),
-async function(req, res, next) {
-  const errors = validationResult(req);
-  var _uuid;
+  body('tbxPassword').notEmpty().isStrongPassword({ minLength: 8, minUppercase: 1, minNumbers: 1, minSymbols: 1 }).withMessage('Please use strong password'),
+  body('tbxConfirmPassword').notEmpty().custom((value, { req }) => { return req.body.tbxPassword === req.body.tbxConfirmPassword }),
+  async function (req, res, next) {
+    const errors = validationResult(req);
+    var _uuid = nanoid();
 
-  console.log(errors);
-  if (!errors.isEmpty()) {
-    return res.render('premises/add', { errors: errors.mapped() });
-  }
+    console.log(errors);
+    if (!errors.isEmpty()) {
+      return res.render('premises/add', { errors: errors.mapped() });
+    }
 
-  req.body.tbxPhone = req.body.tbxPhone.replace(/\D/g, '');
-  var hashedPassword = await bcrypt.hashSync(req.body.tbxPassword, 10);
+    req.body.tbxPhone = req.body.tbxPhone.replace(/\D/g, '');
+    var hashedPassword = await bcrypt.hashSync(req.body.tbxPassword, 10);
 
-  var existPremise = await db('premises')
-  .select('*')
-  .where({ phone_number: req.body.tbxPhone });
+    var existPremise = await db('premises')
+      .select('*')
+      .where({ phone_number: req.body.tbxPhone });
 
-  if (existPremise.length > 0) {
-    _uuid = existPremise[0].uuid;
+    if (existPremise.length > 0) {
+      _uuid = existPremise[0].uuid;
 
-    if(existPremise[0].is_active == true) {
-      return res.render('premises/exists', { phone: existPremise[0].phone_number, business: existPremise[0].business_name });
+      if (existPremise[0].is_active == true) {
+        return res.render('premises/exists', { phone: existPremise[0].phone_number, business: existPremise[0].business_name });
+      } else {
+        await db('premises').update({
+          business_name: req.body.tbxBusiness,
+          phone_number: req.body.tbxPhone,
+          password: hashedPassword
+        })
+      }
     } else {
-      _uuid = nanoid();
-
-      await db('premises').update({
-        business_name: req.body.tbxBusiness,
-        phone_number: req.body.tbxPhone,
-        password: hashedPassword
-      })
-
       await db('premises').insert({
         uuid: _uuid,
         business_name: req.body.tbxBusiness,
@@ -66,20 +65,19 @@ async function(req, res, next) {
         console.log(error);
       })
     }
-  }
 
-  var expiredDateTime = moment().add(1, 'hour').format('YYYY-MM-DD HH:mm:ss');
+    var expiredDateTime = moment().add(1, 'hour').format('YYYY-MM-DD HH:mm:ss');
 
-  await db('phone_verification').insert({
-    phone_number: req.body.tbxPhone,
-    uuid: _uuid,
-    code: Math.floor(100000 + Math.random() * 900000),
-    expired_time: expiredDateTime,
-    status: 'Pending'
-  })
+    await db('phone_verification').insert({
+      phone_number: req.body.tbxPhone,
+      uuid: _uuid,
+      code: Math.floor(100000 + Math.random() * 900000),
+      expired_time: expiredDateTime,
+      status: 'Pending'
+    })
 
-  res.redirect('verification/' + req.body.tbxPhone + '/' + _uuid);
-});
+    res.redirect('verification/' + req.body.tbxPhone + '/' + _uuid);
+  });
 
 router.get('/verification/:phone/:uuid', function (req, res) {
   console.log(req.params);
@@ -92,18 +90,30 @@ router.post('/verification', async function (req, res) {
   var _phone = req.body.tbxPhone;
 
   console.log(req.body);
-
+  
   await db('phone_verification')
-  .where({ uuid: _uuid, code: _code })
-  .orderBy('id', 'desc')
-  .limit(1)
-  .then((result) => {
-    if (result.length == 0) {
-      return res.render('premises/verification', { phone: _phone, uuid: _uuid, error: 'Invalid verification code. Try again.' })
-    } else {
-      return res.send('success');
-    }
-  })
+    .where({ uuid: _uuid, code: _code })
+    .orderBy('id', 'desc')
+    .limit(1)
+    .then(async (result) => {
+      if (result.length == 0) {
+        return res.render('premises/verification', { phone: _phone, uuid: _uuid, error: 'Invalid verification code. Try again.' })
+      } else {
+        await db('phone_verification')
+        .update({ status: 'Verified' })
+        .where({ id: result[0].id })
+        .then((result) => console.log(result))
+        .catch((error) => console.error(error))
+
+        await db('premises')
+        .update({ is_verified: true})
+        .where({ phone_number: _phone})
+        .then((result) => console.log(result))
+        .catch((error) => console.error(error))
+
+        res.redirect('login');
+      }
+    })
 })
 
 module.exports = router;
